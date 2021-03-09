e5d307ee92a670a7a054f1b9ee545718504ce27a
Gabriel Amaro <gabrielamaro4231@gmail.com>
2021-03-09 15:30:48 -0300

    Layout Basico

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,53 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Jokenpo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+      theme: ThemeData(primaryColor: Colors.blue),
+      home: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: Text("IMC"),
+        ),
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Flexible(
+                flex: 5,
+                child: Container(
+                  color: Colors.pink,
+                ),
+              ),
+              Flexible(
+                flex: 3,
+                child: Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.brown,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ]),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
